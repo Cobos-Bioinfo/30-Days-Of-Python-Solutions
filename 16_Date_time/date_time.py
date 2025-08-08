25d6d31dc6ff28a1017aae269caf4c73e1198abb
Cobos <AlejandroCobos2424@gmail.com>
2025-08-08 21:24:21 +0200

    Update Day 16

--- 16_Date_time/date_time.py
+++ 16_Date_time/date_time.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 # 1 - Get the current day, month, year, hour, minute and timestamp from datetime module
 now = datetime.now()
+# ":02d" Adds 0-padding (0=pad with 0, 2=width of 2, d=decimal integer (no decimal points))
 print(
     f"Current Date (dd/mm/yyyy): {now.day:02d}/{now.month:02d}/{now.year}\n"
     f"Current Time (hh:min): {now.hour:02d}:{now.minute:02d}\n"
@@ -12,10 +13,13 @@ print(
 )
 
 # 2 - Format the current date using this format: "%m/%d/%Y, %H:%M:%S")
-
+print(
+    f"Current Date: {now.strftime("%b. %d, %Y")}\n"
+    f"Current Time: {now.strftime("%H:%M:%S")}"
+)
 
 # 3 - Today is 5 December, 2019. Change this time string to time.
-
+print(datetime.strptime("5 December, 2019", "%d %B, %Y"))
 
 # 4 - Calculate the time difference between now and new year.
 
