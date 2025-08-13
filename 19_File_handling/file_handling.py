f8c7103ca768ccc697f2cdf48a444e97cedc0f07
Cobos <AlejandroCobos2424@gmail.com>
2025-08-13 23:25:51 +0200

    Update Day 19

--- 19_File_handling/file_handling.py
+++ 19_File_handling/file_handling.py
@@ -3,5 +3,9 @@
 
 # Level 1
 # 1 - Write a function which count number of lines and number of words in a text. All the files are in the data the folder: a) Read obama_speech.txt file and count number of lines and words b) Read michelle_obama_speech.txt file and count number of lines and words c) Read donald_speech.txt file and count number of lines and words d) Read melina_trump_speech.txt file and count number of lines and words
-def count_lines_words(txt):
-    
+def count_lines_words():
+    with open("./data/obama_speech.txt", "r") as f:
+        lines: list[str] = f.readlines()
+        print(len(lines))
+
+count_lines_words()
