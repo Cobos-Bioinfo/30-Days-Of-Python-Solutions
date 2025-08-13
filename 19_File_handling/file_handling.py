ff8145bbc534ba76d1da485faa6495f6228ed7df
Cobos <AlejandroCobos2424@gmail.com>
2025-08-13 23:07:43 +0200

    Day 19 - Cleaned data

---
+++ 19_File_handling/file_handling.py
@@ -0,0 +1,7 @@
+# Day 19 - 30DaysOfPython Challenge
+# File handling
+
+# Level 1
+# 1 - Write a function which count number of lines and number of words in a text. All the files are in the data the folder: a) Read obama_speech.txt file and count number of lines and words b) Read michelle_obama_speech.txt file and count number of lines and words c) Read donald_speech.txt file and count number of lines and words d) Read melina_trump_speech.txt file and count number of lines and words
+def count_lines_words(txt):
+    
