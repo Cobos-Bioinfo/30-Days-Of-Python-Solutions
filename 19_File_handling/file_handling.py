d2b5a082614928a8f848095d45de01c811b863c2
Cobos <AlejandroCobos2424@gmail.com>
2025-08-18 23:09:26 +0200

    Day 19 & Data update
    Finally finished Day 19 while on vacation and added __init__.py to data to try and work with importing files as modules

--- 19_File_handling/file_handling.py
+++ 19_File_handling/file_handling.py
@@ -2,6 +2,10 @@
 # File handling
 import re
 import json
+import sys
+import csv
+sys.path.append("data")
+from stop_words import stop_words as sw # Used in Level 2-7
 
 # Level 1
 # 1 - Write a function which count number of lines and number of words in a text. All the files are in the data the folder: a) Read obama_speech.txt file and count number of lines and words b) Read michelle_obama_speech.txt file and count number of lines and words c) Read donald_speech.txt file and count number of lines and words d) Read melina_trump_speech.txt file and count number of lines and words
@@ -60,7 +64,7 @@ def find_most_common_words(file_path: str, n: int) -> list[tuple]:
     
     return sorted([(count, word) for word, count in word_count.items()], reverse=True)[:n]
 
-# This was my original approach. However, when refactoring my code, I came across this much better and elegant approach using set():
+# The previous was my original approach. However, when refactoring my code, I came across this different approach using set(). This is significantly slower since list.count() runs in O(n) and is called for every unique word (O(n²) overall). Nevertheless, I will leave it because I thought it was interesting
 def find_most_common_wordsV2(file_path: str, n: int) -> list[tuple]:
     with open(file_path, "r") as f:
         raw_text:str = f.read().lower()
@@ -73,9 +77,55 @@ def find_most_common_wordsV2(file_path: str, n: int) -> list[tuple]:
 
 # 6 - Use the function, find_most_frequent_words to find: a) The ten most frequent words used in Obama's speech b) The ten most frequent words used in Michelle's speech c) The ten most frequent words used in Trump's speech d) The ten most frequent words used in Melina's speech
 print(
-    f"Obama's speech 10 most frequent words:\n{find_most_common_wordsV2("./data/obama_speech.txt", 10)}\n"
-    f"Michelle's speech 10 most frequent words:\n{find_most_common_wordsV2("./data/michelle_obama_speech.txt", 10)}\n"
-    f"Trump's speech 10 most frequent words:\n{find_most_common_wordsV2("./data/donald_speech.txt", 10)}\n"
-    f"Melina's speech 10 most frequent words:\n{find_most_common_wordsV2("./data/melina_trump_speech.txt", 10)}")
+    f"Obama's speech 10 most frequent words:\n{find_most_common_words("./data/obama_speech.txt", 10)}\n"
+    f"Michelle's speech 10 most frequent words:\n{find_most_common_words("./data/michelle_obama_speech.txt", 10)}\n"
+    f"Trump's speech 10 most frequent words:\n{find_most_common_words("./data/donald_speech.txt", 10)}\n"
+    f"Melina's speech 10 most frequent words:\n{find_most_common_words("./data/melina_trump_speech.txt", 10)}")
 
 # 7 - Write a python application that checks similarity between two texts. It takes a file or a string as a parameter and it will evaluate the similarity of the two texts. For instance check the similarity between the transcripts of Michelle's and Melina's speech. You may need a couple of functions, function to clean the text(clean_text), function to remove support words(remove_support_words) and finally to check the similarity(check_text_similarity). List of stop words are in the data directory
+def clean_text(file: str) -> list[str]:
+    with open(file, "r") as f:
+        raw_text: str = f.read().lower()
+
+    return re.findall(r"\b[a-z]+\b", raw_text)
+
+def remove_support_words(all_words: list[str]) -> list[str]:
+    return [word for word in all_words if word not in sw]
+
+# I will use Jaccard similarity = ∣A∩B∣ ​/ ∣A∪B∣
+def check_txt_similarity(text_path1: str, text_path2: str) -> float:
+    words1: set[str] = set(remove_support_words(clean_text(text_path1)))
+    words2: set[str] = set(remove_support_words(clean_text(text_path2)))
+    
+    intersection: set[str] = words1.intersection(words2)
+    union: set[str] = words1.union(words2)
+    
+    return round(len(intersection) / len(union), 2) if union else 0.0
+
+# 8 - Find the 10 most repeated words in the romeo_and_juliet.txt
+print(f"The 10 most repeated words in romeo and juliet are: {find_most_common_words("./data/romeo_and_juliet.txt", 10)}")
+
+# 9 - Read the hacker news csv file and find out: a) Count the number of lines containing python or Python b) Count the number lines containing JavaScript, javascript or Javascript c) Count the number lines containing Java and not JavaScript
+
+def count_languages(file_path: str):
+    py_count = 0
+    js_count = 0
+    java_count = 0
+    
+    with open(file_path, "r") as csvf:
+        csvreader = csv.reader(csvf)
+        for row in csvreader:
+            line = " ".join(row).lower()
+            
+            if "python" in line:
+                py_count += 1
+            
+            if "javascript" in line:
+                js_count += 1
+            
+            if "java" in line and "javascript" not in line:
+                java_count += 1
+    
+    return f"Python count: {py_count}\nJavaScript count: {js_count}\nJava (not JS) count: {java_count}"
+
+print(count_languages("./data/hacker_news.csv"))
